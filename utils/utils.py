3c8458a5db3ef800c3e598f01188b2f57a8ea34e
tevon walker <tjw0018@uah.edu>
2019-12-30 20:36:17 -0500

    Make the extraction of batch variables flexible to any environment

--- utils/utils.py
+++ utils/utils.py
@@ -15,11 +15,11 @@ def correct_rewards(tup, threshold):
         reward = 0
     """
 
-    if abs(tup[0][2]) > threshold:
-        reward = 0
+    if abs(tup[0][2]) < threshold / 2:
+        reward = 1
 
     else:
-        reward = 1
+        reward = 0
     
     return reward
 
@@ -35,18 +35,18 @@ def learn(agent, target, opt, criterion, batch_size):
 
     # Dissect batch
 
-    state_batch = batch[0][:4]
-    action_batch = batch[0][4].view(-1)
-    reward_batch = batch[0][5].view(-1)
-    next_state_batch = batch[0][6:10]
+    state_batch = batch[0][:agent.state_space]
+    action_batch = batch[0][agent.state_space].view(-1)
+    reward_batch = batch[0][agent.state_space + 1].view(-1)
+    next_state_batch = batch[0][agent.state_space + 2:agent.state_space * 2 + 2]
     done_batch = batch[0][-1].view(-1)
 
     for exp in batch:
 
-        state_batch = torch.cat([state_batch, exp[:4]])
-        action_batch = torch.cat([action_batch, exp[4].view(-1)])
-        reward_batch = torch.cat([reward_batch, exp[5].view(-1)])
-        next_state_batch = torch.cat([next_state_batch, exp[6:10]])
+        state_batch = torch.cat([state_batch, exp[:agent.state_space]])
+        action_batch = torch.cat([action_batch, exp[agent.state_space].view(-1)])
+        reward_batch = torch.cat([reward_batch, exp[agent.state_space + 1].view(-1)])
+        next_state_batch = torch.cat([next_state_batch, exp[agent.state_space + 2:agent.state_space * 2 + 2]])
         done_batch = torch.cat([done_batch, exp[-1].view(-1)])
 
     state_batch = state_batch.view(batch_size + 1, -1)[1:]
