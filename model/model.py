864f2f3f4d6e42d35e313377eb5feff13dbd546b
tevon <twalker81@gatech.edu>
2020-04-27 12:35:57 -0400

    Add conv DQN agent

--- model/model.py
+++ model/model.py
@@ -5,7 +5,8 @@ import torch
 import torch.nn as nn
 import numpy as np
 from torch.distributions import Categorical
-from model.replay_buffer import ReplayBuffer
+from model.replay_buffer import ReplayBuffer, ReplayBufferMulti
+import cv2
 
 class DQNAgent(nn.Module):
     def __init__(self, state_space, action_space, eps=0.99, lr=1e-3, buf_len=1000, gamma=0.99, decay=0.99):
@@ -123,3 +124,85 @@ class PGAgent(nn.Module):
     def flush_buffers(self):
         self.rewards = []
         self.log_probs = []
+
+class DQNConvAgent(nn.Module):
+    def __init__(self, state_space, action_space, eps=0.99, lr=1e-3, buf_len=1000, gamma=0.7, decay=0.99):
+        super(DQNConvAgent, self).__init__()
+
+        self.state_space = state_space
+        self.action_space = action_space
+        self.eps = eps
+        self.lr = lr
+        self.replay_memory = ReplayBufferMulti(state_size=state_space, buf_len=buf_len)
+
+        self.conv1 = nn.Conv2d(1, 32, 3, 2)
+        self.conv2 = nn.Conv2d(32, 64, 3, 2)
+        self.conv3 = nn.Conv2d(64, 64, 3, 2)
+        #self.conv4 = nn.Conv2d(64, 64, 3, 2)
+        self.lin1 = nn.Linear(7744, 512)
+        self.lin1_ = nn.Linear(512, 256)
+        self.lin2 = nn.Linear(256, action_space)
+        self.gamma = gamma
+        self.decay = decay
+
+    def store_experience(self, experience):
+        """
+
+        Store experience tuple in replay buffer
+        """
+
+        self.replay_memory.store_experience(experience)
+
+    def forward(self, x):
+        """
+
+        Forward pass of DQN
+        """
+
+        x = torch.relu(self.conv1(x))
+
+        x = torch.relu(self.conv2(x))
+        x = torch.relu(self.conv3(x))
+        #x = torch.relu(self.conv4(x))
+
+        #view = x[0][0].view(14, 15, 1)
+        #view = view.cpu().detach().numpy()
+
+        #cv2.imshow("conv", view)
+        #cv2.waitKey(1)
+        x = x.view(-1)
+
+        x = x.view(-1, 7744)
+
+        x = torch.relu(self.lin1(x))
+        x = torch.relu(self.lin1_(x))
+        x = self.lin2(x)
+
+        return x
+
+
+    def choose_action(self, state):
+        """
+
+        Select action according to q value or random
+        """
+        action = None
+
+        if (self.eps > np.random.rand()): # Select random action if eps happened
+            action = np.random.randint(self.action_space)
+
+            #print ("selecting random action")
+
+        else: # Get q_value with highest action
+            q_values = self.forward(state)
+            action = torch.argmax(q_values).item()
+
+            #print ("selecting greedy action")
+
+
+        return action
+
+
+    def anneal_eps(self):
+        if (self.eps > 0.1):
+            self.eps *= self.decay
